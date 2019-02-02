abd50df0a29422b4cca60e1c97889cf238bbd456
Unknown <luka.furlan9@gmail.com>
2019-02-02 20:32:29 +0100

    fixed exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-  
+import { generatePaymentHash, createPaymentRequest, generateIPNHash } from "./functions";
+import { QUOTE_URL, MERCHANT_URL, REST_API } from './constants';
+
+export const createPaymentHash = generatePaymentHash;
+export const createIPNHash = generateIPNHash;
+export const generatePaymentRequest = createPaymentRequest;
+
+export const constants = {
+  QUOTE_URL,
+  MERCHANT_URL,
+  REST_API,
+};
+
+export enum EnvironmentType {
+  PRODUCTION = 'production',
+  SANDBOX = 'sandbox',
+} 
