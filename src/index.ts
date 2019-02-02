d9a0a232b57328fe09e55a9743adbdee1da2c488
Unknown <luka.furlan9@gmail.com>
2019-02-02 20:24:57 +0100

    main fix

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
-import { generatePaymentHash, createPaymentRequest, generateIPNHash } from "./functions";
-import { QUOTE_URL, MERCHANT_URL, REST_API } from './constants';
-import { EnvironmentType } from "./enums";
-
-export default {
-    generatePaymentHash,
-    generateIPNHash,
-    createPaymentRequest,
-    EnvironmentType,
-    QUOTE_URL,
-    MERCHANT_URL,
-    REST_API,
-}
+  
